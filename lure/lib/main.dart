03b270fbb93b9f85cc319ac0fa1692e27b474c64
Luka De Bakker <lukadebakker5@gmail.com>
2023-01-06 15:27:06 +0100

    🐛 Started designing the login screen.

--- lure/lib/main.dart
+++ lure/lib/main.dart
@@ -1,5 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp()
+  runApp(
+    MaterialApp(
+      title: 'Hashi',
+      home: const HomePage(),
+      theme: ThemeData(
+        fontFamily: 'RobotoMono',
+        scaffoldBackgroundColor: Colors.white,
+      ),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Hashi',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 64,
+                color: Colors.orange,
+              ),
+            ),
+            LoginForm(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Email',
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Password',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+              ),
+              onPressed: (() {
+                // validate form.
+              }),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
